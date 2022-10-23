539c3208fbb798d355e560e30bda035ab7db7ff0
lukiakos <lukiakos@gmail.com>
2022-10-23 16:53:14 +0200

    Basic skeleton of html and css

---
+++ script.js
@@ -0,0 +1,11 @@
+let myLibrary = [];
+
+function Book (title, author, pages, read) {
+    this.author = author;
+    this.title = title;
+    this.pages = pages;
+    this.read = read;
+}
+
+function addBookToLibrary() {
+}
