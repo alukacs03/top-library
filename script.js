531fcf46652b4f68c4ecd713ba1fc292a6d8c71e
Ákos Lukács <81911776+lukiakos@users.noreply.github.com>
2023-01-06 18:43:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,13 @@
 let myLibrary = [];
 
-function Book (title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
+class Book {
+    constructor (title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+    }
+    
 }
 
 
